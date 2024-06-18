272a82ba58158181cd2da7aa776a871968c7a673
griffw00 <97977353+griffw00@users.noreply.github.com>
2024-06-18 16:27:48 -0700

    Added PUT and DELETE mappings

--- src/main/java/com/springapi/rest_demo/controller/CloudVendorAPIService.java
+++ src/main/java/com/springapi/rest_demo/controller/CloudVendorAPIService.java
@@ -3,25 +3,56 @@ package com.springapi.rest_demo.controller;
 import com.springapi.rest_demo.model.CloudVendor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudVendorAPIService {
 
-    CloudVendor cloudVendor;
+    // Dictionary for vendors
+    private Map<String, CloudVendor> cloudVendors = new HashMap<>();;
 
     @GetMapping("{vendorId}")
-    public CloudVendor getCloudVendorDetails(@PathVariable String vendorId) {
+    public CloudVendor getCloudVendorDetails(@PathVariable String vendorId) throws Exception {
 
-        if (cloudVendor == null) {
-            cloudVendor = new CloudVendor("C1", "Vendor 1",
-                    "Address One", "xxxxx");
+
+        if (cloudVendors.get(vendorId) != null) {
+            return cloudVendors.get(vendorId);
+        } else {
+            // Vendor not in dictionary
+            throw new Exception("Vendor not found");
         }
-        return cloudVendor;
     }
 
     @PostMapping("{vendorId}")
     public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
-        this.cloudVendor = cloudVendor;
-        return "Success";
+        CloudVendor cv = new CloudVendor(cloudVendor.getVendorId(),
+                cloudVendor.getVendorName(),
+                cloudVendor.getVendorAddress(),
+                cloudVendor.getVendorPhoneNumber());
+        cloudVendors.put(cloudVendor.getVendorId(), cv);
+        return "Cloud Vendor Added";
     }
+
+    @PutMapping("{vendorId}")
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        if (cloudVendors.get(cloudVendor.getVendorId()) != null) {
+            cloudVendors.put(cloudVendor.getVendorId(), cloudVendor);
+            return "Vendor Updated";
+        } else {
+            return "Vendor Does Not Exist";
+        }
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        if (cloudVendors.get(cloudVendor.getVendorId()) != null) {
+            cloudVendors.remove(cloudVendor.getVendorId());
+            return "Vendor Removed";
+        } else {
+            return "Vendor Does Not Exist";
+        }
+    }
+
 }
